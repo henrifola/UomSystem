14e0e2203f7f368bc8713c4369e9fccc2618b519
August Strømsnes <auguss18@uia.no>
2020-12-14 20:00:30 +0100

    fixed the converting

--- EngineeringUnitCore/Converter/UnitConverter.cs
+++ EngineeringUnitCore/Converter/UnitConverter.cs
@@ -35,6 +35,8 @@ namespace EngineeringUnitsCore.Converter
                 case false when inputBase is false:
                     return await bothCustom(inputUnitId, outputUnitId, quantity);
                 case true when inputBase is true:
+                    if (inputUnitId != outputUnitId)
+                        throw new ArgumentException("Cant convert units with different base unit");
                     var cu = await _unitOfMeasureRepo.Get(outputUnitId);
                     return new ConversionResult(quantity, cu.Id, cu.Annotation);
                 case false when inputBase is true:
@@ -44,10 +46,12 @@ namespace EngineeringUnitsCore.Converter
             }
        return new ConversionResult(0, "unknown", "unknown"); }
         
-        private async Task<ConversionResult> baseInput(string inputUnitId, string outputUnitId, double quantity)
+        private async Task<ConversionResult> baseInput(string inputUnitId, string outputUnitId, double quantity) //base -> customary
         {
             //only need to convert TO customary
-            if (ValidateConversion(inputUnitId, outputUnitId).Result is false) throw new ArgumentException("Cant convert units with different base unit");
+            //if (ValidateConversion(inputUnitId, outputUnitId).Result is false) throw new ArgumentException("Cant convert units with different base unit");
+            var IB = await GetBaseUnit(outputUnitId);
+            if (IB != inputUnitId) throw new ArgumentException("Cant convert units with different base unit");
             var toCustomaryConversion = await ConversionToCustomary(outputUnitId, quantity);
             var cu = await _customaryUnitRepo.Get(outputUnitId);
             var conversionResult = new ConversionResult(toCustomaryConversion, cu.Id, cu.Annotation);
@@ -56,7 +60,9 @@ namespace EngineeringUnitsCore.Converter
 
         private async Task<ConversionResult> baseOutput(string inputUnitId, string outputUnitId, double quantity)
         {
-            if (ValidateConversion(inputUnitId, outputUnitId).Result is false) throw new ArgumentException("Cant convert units with different base unit");
+            //if (ValidateConversion(inputUnitId, outputUnitId).Result is false) throw new ArgumentException("Cant convert units with different base unit");
+            var OB = await GetBaseUnit(inputUnitId);
+            if (OB != outputUnitId ) throw new ArgumentException("Cant convert units with different base unit");
             var toBaseConversion = await ConversionToBase(inputUnitId, quantity);
             var cu = await _unitOfMeasureRepo.Get(outputUnitId);
             var conversionResult = new ConversionResult(toBaseConversion, cu.Id, cu.Annotation);
@@ -65,7 +71,10 @@ namespace EngineeringUnitsCore.Converter
 
         private async Task<ConversionResult> bothCustom(string inputUnitId, string outputUnitId, double quantity)
         {
-            if (ValidateConversion(inputUnitId, outputUnitId).Result is false) throw new ArgumentException("Cant convert units with different base unit");
+            //if (ValidateConversion(inputUnitId, outputUnitId).Result is false) throw new ArgumentException("Cant convert units with different base unit");
+            var IB = await GetBaseUnit(outputUnitId);
+            var OB = await GetBaseUnit(inputUnitId);
+            if (OB != IB) throw new ArgumentException("Cant convert units with different base unit");
             var toBaseConversion = await ConversionToBase(inputUnitId, quantity);
             var toCustomaryConversion = await ConversionToCustomary(outputUnitId, toBaseConversion);
             var cu = await _customaryUnitRepo.Get(outputUnitId);
@@ -100,19 +109,26 @@ namespace EngineeringUnitsCore.Converter
         }
         private async Task<double> ConversionToCustomary(string unit, double baseConversion)
         {
-            if (_memoryCache.TryGetValue(unit, out ConversionToBaseUnit cacheOut)) return ConversionCalculation(cacheOut.A, cacheOut.C, cacheOut.B, cacheOut.D, baseConversion);
+            //if (_memoryCache.TryGetValue(unit, out ConversionToBaseUnit cacheOut)) return ConversionCalculation(cacheOut.A, cacheOut.C, cacheOut.B, cacheOut.D, baseConversion);
+            if (_memoryCache.TryGetValue(unit, out ConversionToBaseUnit cacheOut)) return ConversionCalculationToCustomary(cacheOut.A, cacheOut.B, cacheOut.C, cacheOut.D, baseConversion);
             Console.WriteLine("Customary conversion not cached, caching now");
             
             cacheOut = await GetCacheUnit(unit);
             var cacheEntryOptions = new MemoryCacheEntryOptions();
             _memoryCache.Set(unit, cacheOut, cacheEntryOptions);
-            return ConversionCalculation(cacheOut.A, cacheOut.C, cacheOut.B, cacheOut.D, baseConversion);
+            //return ConversionCalculation(cacheOut.A, cacheOut.C, cacheOut.B, cacheOut.D, baseConversion);
+            return ConversionCalculationToCustomary(cacheOut.A, cacheOut.B, cacheOut.C, cacheOut.D, baseConversion);
         }
         //swap b and c when going from base to customary unit, and insert base conversion as x
         private static double ConversionCalculation(double a, double b, double c, double d, double x)
         {
             return (a + (b * x)) / (c + (d * x));
         }
+        private static double ConversionCalculationToCustomary(double a, double b, double c, double d, double x)
+        {
+            return (a - (c * x)) / ( (d * x) - b);
+        }
+        
         
         private async Task<ConversionToBaseUnit> GetCacheUnit(string unit)
         {
